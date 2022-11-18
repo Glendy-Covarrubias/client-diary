a7155a0ce856878ff3c8a77b598937d07366654d
Glendy-Covarrubias <glendycovarrubias@hotmail.com>
2022-11-17 21:36:30 -0600

    Incorporando componente de tabla tapa inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import {MatTableModule } from '@angular/material/table';
+
+
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/generals/table/table.component';
+import { DiaryComponent } from './components/diary/diary.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    DiaryComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule
   ],
+  exports: [],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
